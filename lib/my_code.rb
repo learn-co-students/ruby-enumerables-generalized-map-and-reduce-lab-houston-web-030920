26073dc3059e0a6fab9f702cd19cc59cfc6bd34e
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 00:17:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  #So we're literally creating what map does
+  arr = []
+  source_array.each do |e|
+    arr << yield(e)
+  end
+  arr
+end
+
+
+def reduce(array, sv = nil)
+  if sv
+    sum = sv
+    array.each do |e|
+      sum = yield(sum, e)
+    end
+  else
+    sum = 0
+    array.each do |e|
+      sum = yield(sum,e)
+    end
+  end
+  sum
+end
