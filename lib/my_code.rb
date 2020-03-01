02ce48054809e992b7d4f5456da4c69e9b4a5af4
FAIZAH AHSAN <faizah1992@gmail.com>
2020-03-01 19:25:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map (array)
+  new=[]
+  i=0 
+  while i<array.length 
+  new.push(yield(array[i]))
+  i += 1 
+  end 
+  new
+end
+
+ 
+def reduce (array, sv=nil)
+  if sv 
+    result= sv 
+    i = 0 
+  else 
+    result= array[0]
+    i = 1 
+  end 
+  while i < array.length 
+  result = yield(result, array[i])
+  i += 1 
+  end 
+  result
+end 
+
+
